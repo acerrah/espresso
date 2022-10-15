b438fc8e92e0eeb0be68203a20b191acbe70edd0
acerrah <37017703+acerrah@users.noreply.github.com>
2022-10-15 04:55:04 +0300

    added notes

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ echo "\nalias espresso='bash $(pwd)/espresso.sh'" >> "$shell_f"
 make -C ./lolcat
 make -C ./sl
 source "$shell_f"
+echo "have fun with it by typing espresso"|./lolcat/lolcat
+echo "if it doesn't work try reopening your terminal"|./lolcat/lolcat
